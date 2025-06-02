e6a2ff764998a5335ffc9ffd707798e85fd4da39
Khornne <maurice.pattinsonn@gmail.com>
2025-06-02 11:37:47 +0100

    Created Port class and Parse Function

--- Portscanner.py
+++ Portscanner.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+#! /usr/bin/env python
+import argparse
+
+from colorama import init
+from scapy.all import *
+
+# Colorama init
+init(autoreset=True)
+
+
+class PortScanner:
+    def __init__(self, target_ip, ports):
+        self.target_ip = target_ip
+        self.ports = ports
+
+
+def parse_ports(self, port_input):
+    ports = set()
+    for ports in port_input.split(","):
+        if "-" in part:
+            start_port, end_port = part.split("-")
+            ports.update(range(int(start_port), int(end_port) + 1))
+        else:
+            ports.add(int(part))
+    return sorted(ports)
